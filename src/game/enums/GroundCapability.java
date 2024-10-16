3ea459fa833cd448f546c4498a4245f12bcc70c8
WY <109936014+ventusus@users.noreply.github.com>
2024-10-16 09:45:12 +0800

    Delete src/game directory

--- src/game/enums/GroundCapability.java
+++
@@ -1,9 +0,0 @@
-package game.enums;
-
-/**
- * Enum to represent the ground's capabilities.
- */
-public enum GroundCapability {
-  BLOCKED,
-  TRANSPORTABLE
-}
--- src/game/enums/Weather.java
+++
@@ -1,9 +0,0 @@
-package game.enums;
-
-/**
- * Enum representing the Weather status of the game
- */
-public enum Weather {
-  SUNNY,
-  RAINY
-}
